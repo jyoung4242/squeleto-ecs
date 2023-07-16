a420117ad99ac684cbb67199d31e97f21dc0be0b
Justin Young <justin_dean_young@yahoo.com>
2023-07-16 13:02:04 -0400

    got a component working

--- src/components/sampleComponent.ts
+++ src/components/sampleComponent.ts
@@ -1,15 +1,22 @@
-// list-item.js
+// sampleComponent.ts
 import { UI } from "@peasy-lib/peasy-ui";
 
 export class SampleComponent {
-  static template = "<span>Hello I'm a sample component: ${name}!</span>"; // This can also be a template element
-  name: string;
-
-  constructor(name: string) {
-    this.name = name;
+  public static template = `
+  <style>
+  .\${cssClass}{
+    border: 1px red solid;
+    position: fixed;
+    top: 15px;
+    left: 15px;
+    padding: 10px;
   }
-  static create(state: any) {
-    return new SampleComponent(state.name);
+  </style>
+  <sample-component class="\${cssClass}">Hello I\'m a sample component: \${name}!</sample-component>`;
+
+  constructor(public name: string, public cssClass: string) {}
+
+  public static create(state: { name: string; cssClass: string }): SampleComponent {
+    return new SampleComponent(state.name, state.cssClass);
   }
 }
-UI.register("SampleComponent", SampleComponent); // Can be replaced with a property on invoking model
--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,17 @@ import { UI } from "@peasy-lib/peasy-ui";
 import { SampleComponent } from "./components/sampleComponent";
 
 const model = {
-  names: ["bobs", "your", "uncle"],
+  SampleComponent,
+  compState: {
+    name: "bob",
+    cssClass: "mySampleComponent",
+  },
 };
 
 const template = `
-<template>
-    <div>
-        <div pui="name <=* names"><sample-component pui="SampleComponent === name"></sample-component></div>
+    <div>  
+        <\${SampleComponent === compState}>        
     </div>
-</template>
 `;
 
 await UI.create(document.body, model, template).attached;
-console.log(`Hello World`);
-
-/*
-<rendered-systems \${system<=*systems}></rendered-systems>
-<rendered-entities \${entity<=*entities}></rendered-entities>
-*/
